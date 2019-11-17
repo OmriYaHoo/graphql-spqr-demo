2cbca5a4fce39275a1aefa4f5a9b3d9ea5cd2227
omri.ratson <omri.ratson@viaccess-orca.com>
2019-11-17 15:01:11 +0200

    init commit

---
+++ src/test/java/com/omriyahoo/graphqlspqr/GraphqlSpqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omriyahoo.graphqlspqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
