eb0f8b80a9e7e1d921018bf32f1fef9aa529b804
OmriYaHoo <omriyahoo@gmail.com>
2025-06-16 14:25:55 +0300

    Add GraphQLSchemaController and update dependencies in pom.xml

---
+++ src/main/java/com/omriyahoo/graphqlspqr/controllers/GraphQLSchemaController.java
@@ -0,0 +1,26 @@
+package com.omriyahoo.graphqlspqr.controllers;
+
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.SchemaPrinter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class GraphQLSchemaController {
+
+    private final GraphQL graphQL;
+
+    @Autowired
+    public GraphQLSchemaController(GraphQL graphQL) {
+        this.graphQL = graphQL;
+    }
+
+    @GetMapping(value = "/schema", produces = "application/graphql-response+json")
+    public Mono<String> getSchema() {
+        GraphQLSchema schema = graphQL.getGraphQLSchema();
+        return Mono.just(new SchemaPrinter().print(schema));
+    }
+}
